fa6f9968c79f2a675b3fe9153f28baeeaa1dd8c0
chansenbbai <chanchu@hansung.ac.kr>
2021-07-26 23:46:58 +0900

    회원가입 두번째 뷰

---
+++ BoostCourse_week2/BoostCourse_week2/Source/ViewController/SignUpFirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignUpFirstViewController.swift
+//  BoostCourse_week2
+//
+//  Created by 김승찬 on 2021/07/26.
+//
+
+import UIKit
+
+class SignUpFirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
