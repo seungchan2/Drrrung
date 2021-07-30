54ce241c0dcf89ffdab03329e730d125f48fd040
chansenbbai <chanchu@hansung.ac.kr>
2021-07-30 23:18:09 +0900

    DatePicker 구현 (세부사항 수정 해야..)

--- BoostCourse_week2/BoostCourse_week2/Source/ViewController/SignUpSecondViewController.swift
+++ BoostCourse_week2/BoostCourse_week2/Source/ViewController/SignUpSecondViewController.swift
@@ -9,21 +9,33 @@ import UIKit
 
 class SignUpSecondViewController: UIViewController {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    let dateFormmater: DateFormatter = {
+        let formatter: DateFormatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM//dd"
+        return formatter
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        datePickerAction()
+        
+    
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+            
+            let date: Date = self.datePicker.date
+            let dateString: String = self.dateFormmater.string(from: date)
+            
+            self.dateLabel.text = dateString
     }
-    */
 
+   // 바뀌었을 때 ValueChange
+    
+    private func datePickerAction() {
+        
+        self.datePicker.addTarget(self, action: #selector(self.datePickerValueChanged(_:)), for: UIControl.Event.valueChanged)
+      
+    }
 }
