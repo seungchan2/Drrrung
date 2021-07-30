44c66929eea8185b98000a37a63eaea9524034dd
chansenbbai <chanchu@hansung.ac.kr>
2021-07-30 22:23:17 +0900

    textFieldDidEndEditing 추가

--- BoostCourse_week2/BoostCourse_week2/Source/ViewController/SignUpFirstViewController.swift
+++ BoostCourse_week2/BoostCourse_week2/Source/ViewController/SignUpFirstViewController.swift
@@ -6,21 +6,41 @@
 //
 
 import UIKit
-import Foundation
 
-// 주석달기
+
 class SignUpFirstViewController: UIViewController {
     
+    
+    // Mark: - @IBOutlet Property
+    
     @IBOutlet weak var personImageView: UIImageView!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var checkPasswordTextField: UITextField!
+    @IBOutlet weak var nextPageButton: UIButton!
+    
+    // Mark: - Property
     
     let imagePicker = UIImagePickerController()
 
+    // Mark: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         pickImage()
         imagePickerAttribute()
-      
     }
+    
+    @IBAction func touchNextButton(_ sender: Any) {
+        
+        let signupSecondStoryboard = UIStoryboard(name: Const.Storyboard.Name.signUpSecond, bundle: nil)
+      
+        guard let signUpSecondViewController = signupSecondStoryboard.instantiateViewController(withIdentifier: Const.ViewController.identifier.signUpSecond) as? SignUpSecondViewController else { return }
+     
+        self.navigationController?.pushViewController(signUpSecondViewController, animated: true)
+    }
+    
+    // Mark: - Functions
     
     @objc func pickImage() {
         self.present(self.imagePicker, animated: true)
@@ -46,7 +66,16 @@ class SignUpFirstViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
         
     }
+    
+    private func assignDelegate() {
+        idTextField.delegate = self
+        passwordTextField.delegate = self
+        checkPasswordTextField.delegate = self
+    }
+    
 }
+
+// Mark: - Extension
 
 extension SignUpFirstViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -63,5 +92,27 @@ extension SignUpFirstViewController: UIImagePickerControllerDelegate, UINavigati
         self.personImageView.image = newImage
         picker.dismiss(animated: true, completion: nil)
 }
+
+}
+
+
+extension SignUpFirstViewController: UITextFieldDelegate {
+    
+  func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        
+    if idTextField.text != "" && passwordTextField.text != "" && checkPasswordTextField.text != "" {
+        
+        if passwordTextField.text == checkPasswordTextField.text {
+            nextPageButton.isEnabled = true
+        } else {
+            nextPageButton.isEnabled = false
+        }
+            
+    } else {
+        nextPageButton.isEnabled = false
+    }
+        
+       
+    }
 
 }
