cbb3f0da8fd2eccb0be2b72ae2787f9585e516cc
chansenbbai <chanchu@hansung.ac.kr>
2021-07-31 14:57:07 +0900

    initial commit

---
+++ BoostCourse_Week3/BoostCourse_Week3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BoostCourse_Week3
+//
+//  Created by 김승찬 on 2021/07/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("asd")
+    }
+
+
+}
+
