9729ea30662f7e98e131c53dc9cc3f6c2f38a496
chansenbbai <chanchu@hansung.ac.kr>
2021-08-04 02:07:09 +0900

    UserInformatiton
    Description
    
    - 싱글턴 패턴 연습

--- BoostCourse_week2/BoostCourse_week2/Source/Model/UserInformation.swift
+++ BoostCourse_week2/BoostCourse_week2/Source/Model/UserInformation.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+class UserInformation {
+    static let shared: UserInformation = UserInformation()
+    
+    var id: String?
+    var password: String?
+    var phoneNumber: String?
+    var dateOfBirth: String?
+    
+}
