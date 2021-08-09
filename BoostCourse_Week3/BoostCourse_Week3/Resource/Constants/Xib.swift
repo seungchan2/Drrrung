6e80ee0301fc529774b49a5f8c9ccde038f5d864
chansenbbai <chanchu@hansung.ac.kr>
2021-08-09 16:58:47 +0900

    Xib

---
+++ BoostCourse_Week3/BoostCourse_Week3/Resource/Constants/Xib.swift
@@ -0,0 +1,19 @@
+//
+//  Xib.swift
+//  BoostCourse_Week3
+//
+//  Created by 김승찬 on 2021/08/09.
+//
+
+import Foundation
+
+extension Const {
+    
+    struct Xib {
+        
+        struct Name {
+            
+            static let CountryTableViewCell = "CountryTableViewCell"
+        }
+    }
+}
