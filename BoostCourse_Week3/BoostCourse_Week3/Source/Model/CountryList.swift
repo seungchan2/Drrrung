6fd5abf96a9cce7504b08a4c1c7a2c65f2bd81d7
chansenbbai <chanchu@hansung.ac.kr>
2021-08-09 16:58:25 +0900

    CountryList

---
+++ BoostCourse_Week3/BoostCourse_Week3/Source/Model/CountryList.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherList.swift
+//  BoostCourse_Week3
+//
+//  Created by 김승찬 on 2021/08/09.
+//
+
+import Foundation
+import UIKit
+
+struct CountryList {
+    var iconImageName : String
+    var title : String
+}
