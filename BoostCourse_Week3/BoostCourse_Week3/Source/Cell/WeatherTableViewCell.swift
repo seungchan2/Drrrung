56989d2a84607447902c118c30e6aa9b0d49a92d
chansenbbai <chanchu@hansung.ac.kr>
2021-08-11 23:07:12 +0900

    WeatherTableViewCell

--- BoostCourse_Week3/BoostCourse_Week3/Source/Cell/WeatherTableViewCell.swift
+++ BoostCourse_Week3/BoostCourse_Week3/Source/Cell/WeatherTableViewCell.swift
@@ -8,16 +8,20 @@
 import UIKit
 
 class WeatherTableViewCell: UITableViewCell {
-
+    
+    static let identifier: String = "WeatherTableViewCell"
+    
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var degreeLabel: UILabel!
+    @IBOutlet weak var rainPercentageLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
     
 }
