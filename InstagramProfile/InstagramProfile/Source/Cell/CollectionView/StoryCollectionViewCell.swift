5d204235c0ac9219f986c0880685428de962a968
chansenbbai <chanchu@hansung.ac.kr>
2021-08-18 20:14:45 +0900

    StoryCollection

---
+++ InstagramProfile/InstagramProfile/Source/Cell/CollectionView/StoryCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  StoryCollectionViewCell.swift
+//  InstagramProfile
+//
+//  Created by 김승찬 on 2021/08/18.
+//
+
+import UIKit
+
+class StoryCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier: String = "StoryCollectionViewCell"
+
+    @IBOutlet weak var storyImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    func storyData(imageName: String) {
+        if let image = UIImage(named: imageName)
+        {
+            storyImage.image = image
+        }
+    }
+}
