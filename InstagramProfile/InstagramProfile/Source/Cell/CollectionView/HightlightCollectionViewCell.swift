728f01914a457e7a7e9802787d55248e3bab5a89
chansenbbai <chanchu@hansung.ac.kr>
2021-08-24 19:54:11 +0900

    HightlightCollectionViewCell

--- InstagramProfile/InstagramProfile/Source/Cell/CollectionView/HightlightCollectionViewCell.swift
+++ InstagramProfile/InstagramProfile/Source/Cell/CollectionView/HightlightCollectionViewCell.swift
@@ -13,10 +13,12 @@ class HightlightCollectionViewCell: UICollectionViewCell {
     
     static let identifier: String = "HightlightCollectionViewCell"
     var imageList : [ImageList] = []
-    var isOpened: Bool = false
+    static var isOpened: Bool = true
+
     
     //MARK: @IBOutlets
     
+    @IBOutlet weak var arrowButton: UIButton!
     @IBOutlet weak var hightlightView: UIView!
     @IBOutlet weak var arrowImageView: UIImageView!
     
@@ -36,19 +38,18 @@ class HightlightCollectionViewCell: UICollectionViewCell {
         ])
     }
     
-    func open() {
+    func isOpen() {
         print("open")
         arrowImageView.transform = CGAffineTransform(rotationAngle: .pi)
-    
         self.hightlightView.layoutIfNeeded()
-        isOpened = true
+        HightlightCollectionViewCell.isOpened = true
     }
 
-    func close() {
+    func isClose() {
         print("close")
         arrowImageView.transform = CGAffineTransform(rotationAngle: 0)
         self.hightlightView.layoutIfNeeded()
-        isOpened = false
+        HightlightCollectionViewCell.isOpened = false
 
     }
     
@@ -56,53 +57,14 @@ class HightlightCollectionViewCell: UICollectionViewCell {
     
     @IBAction func seeBottomViewButton(_ sender: Any) {
         
-//        UIView.animate(withDuration: 0.5) {
-//            if self.isOpened{
-//                self.close()
-//            } else {
-//                self.open()
-//            }
-//        }
+        UIView.animate(withDuration: 0.5) {
+            if HightlightCollectionViewCell.isOpened == true {
+                self.isClose()
+            } else {
+                self.isOpen()
+        
+            }
+        }
     }
 }
-
-//MARK: Extensions
-
-//extension HightlightCollectionViewCell: UICollectionViewDelegate {
-//
-//}
-//
-//extension HightlightCollectionViewCell: UICollectionViewDataSource {
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return imageList.count
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        var cell = UICollectionViewCell()
-//
-//        cell = hightlightCollectionView.dequeueReusableCell(withReuseIdentifier: "StoryCollectionViewCell", for: indexPath) as! StoryCollectionViewCell
-//
-//        return cell
-//    }
-//}
-//
-//extension HightlightCollectionViewCell: UICollectionViewDelegateFlowLayout{
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//
-//        let width = UIScreen.main.bounds.width
-//
-//        let cellwidth = 50
-//        let cellheight = 50
-//
-//        return CGSize(width: cellwidth, height: cellheight)
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-//        return 0
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-//        return 3
-//    }
-//}
 
